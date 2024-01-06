7f15e0c28db546f72031fa9b371e1318fe640322
ramshankartv <tvramshankar@gmail.com>
2024-01-06 12:26:28 +0530

    saa

---
+++ MangoAPI/Models/Coupon.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace MangoAPI.Models
+{
+	public class Coupon
+	{
+		public int CouponId { get; set; }
+		public string CouponCode { get; set; }
+		public double DiscountAmount { get; set; }
+		public int MinAmount { get; set; }
+	}	
+}
+
---
+++ MangoAPI/Models/DTO/CouponDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace MangoAPI.Models.DTO
+{
+	public class CouponDTO
+	{
+        public int CouponId { get; set; }
+        public string CouponCode { get; set; }
+        public double DiscountAmount { get; set; }
+        public int MinAmount { get; set; }
+    }
+}
+
