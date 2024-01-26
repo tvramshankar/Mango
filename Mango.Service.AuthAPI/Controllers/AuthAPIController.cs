c3d977ebd8fd3729d38e013b08f2e2f6d111a981
ramshankartv <tvramshankar@gmail.com>
2024-01-26 13:55:42 +0530

    Added AuthAPI Controller

---
+++ Mango.Service.AuthAPI/Controllers/AuthAPIController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Service.AuthAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthAPIController : ControllerBase
+    {
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register()
+        {
+            return Ok();
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login()
+        {
+            return Ok();
+        }
+
+    }
+}
