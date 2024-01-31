e74cafcfb771fdace7cc266313309fdbe7a27f7e
ramshankartv <tvramshankar@gmail.com>
2024-01-31 12:26:23 +0530

    Design of home page for login/register completed

---
+++ Mango.Web/Controllers/AuthController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Mango.Web.Models.DTO;
+using Mango.Web.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            LoginRequestDTO loginRequestDTO = new(); 
+            return View(loginRequestDTO);
+        }
+
+        [HttpPost]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            return View();
+        }
+    }
+}
