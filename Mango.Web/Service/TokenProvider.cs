e03f5e7bddef766ec1417de4c7bbf0c749846fe1
ramshankartv <tvramshankar@gmail.com>
2024-01-31 17:01:13 +0530

    done saving token in cookie functionality

---
+++ Mango.Web/Service/TokenProvider.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Mango.Web.Service.IService;
+using NuGet.Common;
+
+namespace Mango.Web.Service
+{
+	public class TokenProvider : ITokenProvider
+	{
+        private readonly IHttpContextAccessor _httpContextAccessor;
+		public TokenProvider(IHttpContextAccessor httpContextAccessor)
+		{
+            _httpContextAccessor = httpContextAccessor;
+		}
+
+        public void ClearToken()
+        {
+            _httpContextAccessor.HttpContext!.Response.Cookies.Delete(Utility.StaticDetails.TokenCookie);
+        }
+
+        public string? GetToken()
+        {
+            string? token = null;
+            bool? hasToken = _httpContextAccessor.HttpContext?.Request.Cookies.TryGetValue(Utility.StaticDetails.TokenCookie,
+                out token);
+            return hasToken is true ? token : null;
+        }
+
+        public void SetToken(string token)
+        {
+            _httpContextAccessor.HttpContext?.Response.Cookies.Append(Utility.StaticDetails.TokenCookie, token);
+        }
+    }
+}
+
