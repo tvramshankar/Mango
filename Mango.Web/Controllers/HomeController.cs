716b615c76f4a44fcddf20e15e6e32584302904a
ramshankartv <tvramshankar@gmail.com>
2024-03-03 10:22:14 +0530

    done details page which populates products

--- Mango.Web/Controllers/HomeController.cs
+++ Mango.Web/Controllers/HomeController.cs
@@ -1,21 +1,67 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Mango.Web.Models;
+using Newtonsoft.Json;
+using Mango.Web.Models.DTO;
+using Mango.Web.Service.IService;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Mango.Web.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+    private readonly IProductService _productsService;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IProductService productService)
     {
-        _logger = logger;
+        _productsService = productService;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        ServiceResponce<List<ProductsDTO>> responceData = new();
+        try
+        {
+            ServiceResponce<object> responce = await _productsService.GetAllProductAsync();
+            if (responce is not null && responce.IsSuccess)
+            {
+                responceData.Data = JsonConvert.DeserializeObject<List<ProductsDTO>>(responce.Data!.ToString()!);
+            }
+            else
+            {
+                TempData["error"] = responce!.Message;
+            }
+        }
+        catch (Exception ex)
+        {
+            responceData.IsSuccess = false;
+            responceData.Message = ex.Message;
+            TempData["error"] = responceData.Message;
+        }
+        return View(responceData);
+    }
+
+    [Authorize]
+    public async Task<IActionResult> Details(Guid productId)
+    {
+        ProductsDTO data = new();
+        try
+        {
+            ServiceResponce<object> responce = await _productsService.GetProductByIdAsync(productId);
+            if (responce is not null && responce.IsSuccess)
+            {
+                data = JsonConvert.DeserializeObject<ProductsDTO>(responce.Data!.ToString()!)!;
+            }
+            else
+            {
+                TempData["error"] = responce!.Message;
+            }
+        }
+        catch
+        {
+            TempData["error"] = "Something went wrong";
+        }
+        return View(data);
     }
 
     public IActionResult Privacy()
--- Mango.Web/Models/DTO/ProductsDTO.cs
+++ Mango.Web/Models/DTO/ProductsDTO.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace Mango.Web.Models.DTO
 {
 	public class ProductsDTO
@@ -9,6 +11,8 @@ namespace Mango.Web.Models.DTO
         public string Description { get; set; } = string.Empty;
         public string CategoryName { get; set; } = string.Empty;
         public string ImageUrl { get; set; } = string.Empty;
+        [Range(1,100)]
+        public int ProductCount { get; set; } = 1;
     }
 }
 
